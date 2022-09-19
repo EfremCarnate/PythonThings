d54880c6187c8ed55a80adba4aa5c16111c140b2
EfremCarnate <ecar239@aucklanduni.ac.nz>
2022-09-20 10:39:59 +1200

    general relativity

---
+++ GR.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import scipy 
+
+from scipy.integrate import solve_ivp 
+
+F = lambda t, s: -s 
+
+t_eval = np.arange(0, 2.01, 0.01) 
+sol = solve_ivp(F, [0, 2], [1], t_eval=t_eval) 
+
+plt.figure(figsize = (12, 4)) 
+plt.plot(sol.t, sol.y[0]) 
+plt.xlabel('t') 
+plt.ylabel('S(t)') 
+plt.show() 
